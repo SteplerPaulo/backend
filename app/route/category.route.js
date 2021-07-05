5d5993e88015e8816a7f3d566b3616576033b449
SteplerPaulo <paulobiscocho@gmail.com>
2021-07-05 20:07:57 +0800

    Update Category Route

--- app/route/category.route.js
+++ app/route/category.route.js
@@ -1,11 +1,14 @@
-module.exports = function(app) {
- 
+module.exports = function (app) {
+
     const categories = require('../controller/categories.controller.js');
- 
-    // Retrieve all Products
+
+    // Retrieve all
     app.get('/api/categories/all', categories.findAll);
 
 
     // Retrieve by ID
     app.get('/api/categories/:id', categories.findByID);
+
+    // Retrieve per page
+    app.get('/api/categories', categories.findAllQuery);
 }
