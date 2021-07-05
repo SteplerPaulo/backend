b40e14078372a80f79f31ecab30e3c8e916490e6
SteplerPaulo <paulobiscocho@gmail.com>
2021-07-05 20:08:25 +0800

    Update Banner route

--- app/route/banner.route.js
+++ app/route/banner.route.js
@@ -1,11 +1,14 @@
-module.exports = function(app) {
- 
+module.exports = function (app) {
+
     const banners = require('../controller/banners.controller.js');
- 
-    // Retrieve all Products
+
+    // Retrieve all
     app.get('/api/banners/all', banners.findAll);
 
 
     // Retrieve by ID
     app.get('/api/banners/:id', banners.findByID);
+
+    // Retrieve per page
+    app.get('/api/banners', banners.findAllQuery);
 }
