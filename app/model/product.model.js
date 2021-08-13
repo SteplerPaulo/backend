54b03b4366c7f57edbf5003f0c2e7276fe1b6566
SteplerPaulo <paulobiscocho@gmail.com>
2021-08-13 22:29:06 +0800

    Update Product Model

--- app/model/product.model.js
+++ app/model/product.model.js
@@ -8,6 +8,9 @@ module.exports = (sequelize, Sequelize) => {
 	  name: {
 		  type: Sequelize.STRING
 	  },
+	  description: {
+		  type: Sequelize.STRING
+	  },
 	  price: {
 		  type: Sequelize.STRING
 	  },
