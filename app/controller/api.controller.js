cedcf1b1572983385a27355a4de6b298932650b8
SteplerPaulo <paulobiscocho@gmail.com>
2021-08-13 22:29:27 +0800

    Update Api Controller

--- app/controller/api.controller.js
+++ app/controller/api.controller.js
@@ -1,67 +1,82 @@
-const db = require('../config/db.config.js');
+const db = require("../config/db.config.js");
 const Op = db.Sequelize.Op;
 
 exports.findAllQuery = (req, res) => {
-	const Model = db[req.params.model]
-	
-	const { page, size, query } = req.query;
-	const condition = getCondition(req.query, Op)
+  const Model = db[req.params.model];
 
-	const { limit, offset } = getPagination(page, size);
+  const { page, size, query } = req.query;
+  const condition = getCondition(req.query, Op);
 
-	Model.findAndCountAll({
-		where: condition,
-		limit: limit,
-		offset: offset,
-		order: [['id', 'ASC']],
-		include: { all: true }
-	}).then(data => {
-		const response = getPagingData(data, page, limit);
-		res.send(response);
-	});
+  const { limit, offset } = getPagination(page, size);
+  console.log(condition)
 
+  Model.findAndCountAll({
+    where: condition,
+    limit: limit,
+    offset: offset,
+    order: [["id", "ASC"]],
+    include: { all: true },
+  }).then((data) => {
+    const response = getPagingData(data, page, limit);
+    res.send(response);
+  });
+};
+
+exports.postData = (req, res) => {
+  const Model = db[req.params.model];
+  console.log(req.body);
+  Model.create(req.body).then((data) => {
+    console.log(data);
+
+    res.send(data);
+  });
 };
 
 const getPagination = (page, size) => {
-	const limit = size ? +size : 12;
-	const offset = page ? ((page - 1) * limit) : 0;
-	return { limit, offset };
+  const limit = size ? +size : 12;
+  const offset = page ? (page - 1) * limit : 0;
+  return { limit, offset };
 };
 
 const getPagingData = (data, page, limit) => {
-	const { count: totalItems, rows: rows } = data;
-	const currentPage = page ? +page : 0;
-	const totalPages = Math.ceil(totalItems / limit);
+  const { count: totalItems, rows: rows } = data;
+  const currentPage = page ? +page : 0;
+  const totalPages = Math.ceil(totalItems / limit);
 
-	return { totalItems, rows, totalPages, currentPage };
+  return { totalItems, rows, totalPages, currentPage };
 };
 
 const getCondition = (query, Op) => {
-	const array = Object.entries(query)
-	let condition = {}
+  const array = Object.entries(query);
+  let condition = {};
+  array.filter((item, index) => {
+    if (item[0].match(/name/i)) {
+      condition[item[0]] = {};
+      condition[item[0]][Op.like] = `%${item[1]}%`;
+      return false;
+    }
+    if (item[0].match(/price/i)) {
+      condition[item[0]] = {};
+      if (item[1].match(/[\-\w]+\-[\-\w]+/)) {
+        condition[item[0]][Op.between] = item[1].split("-").map(Number);
+        return false;
+      }
 
-	array.filter((item, index) => {
-		if (item[0].match(/name/i)) {
-			condition[item[0]] = {}
-			condition[item[0]][Op.like] = `%${item[1]}%`
-			return false
-		}
-		if (item[0].match(/price/i)) {
-
-			condition[item[0]] = {}
-			if (item[1].match(/[\-\w]+\-[\-\w]+/)) {
-				condition[item[0]][Op.between] = item[1].split('-').map(Number)
-				return false
-			}
-
-			if (item[1].match(/\b-/i)) {
-				condition[item[0]][Op.gte] = parseInt(item[1].replace(/\-/, ''))
-				return false
-			} else if (item[1].match(/\B-/i)) {
-				condition[item[0]][Op.lte] = parseInt(item[1].replace(/\-/, ''))
-				return false
-			}
-		}
-	})
-	return condition
-}
+      if (item[1].match(/\b-/i)) {
+        condition[item[0]][Op.gte] = parseInt(item[1].replace(/\-/, ""));
+        return false;
+      } else if (item[1].match(/\B-/i)) {
+        condition[item[0]][Op.lte] = parseInt(item[1].replace(/\-/, ""));
+        return false;
+      }
+    }
+   // if (item[0].match(/^page/i)) {
+     // condition[item[0]] = {};
+     // condition[item[0]][Op.like] = `%${item[1]}%`;
+      //return false;
+    //}
+    
+    
+  });
+  return condition;
+};
